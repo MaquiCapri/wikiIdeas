cf5603b781ff883285bcaf670ac8824ca46d0c5c
maquiCapri <maquicapri@gmail.com>
2022-11-04 01:29:12 -0300

    comienzo del proyectos. Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { ContentsComponent } from './components/contents/contents.component';
+import { IndexComponent } from './components/index/index.component';
+import { ToolsComponent } from './components/tools/tools.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsideComponent,
+    ContentsComponent,
+    IndexComponent,
+    ToolsComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

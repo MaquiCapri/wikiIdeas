6ec0f84e1a89244fbedef94251cf445f4aa6b4a6
maquiCapri <maquicapri@gmail.com>
2022-11-24 08:34:30 -0300

    Auto-generated commit [ci skip]

---
+++ assets/home.js
@@ -0,0 +1,26 @@
+
+
+  $(document).ready(function(){
+          $(".list").hide();
+          $(".list1").hide();
+          $(".list2").hide();
+          $(".list3").hide();
+
+          $("#cultura").hover(function(){$(".list").show();
+           $("#culture").hide();
+}, function(){$(".list").hide();
+$("#culture").show();});
+
+$("#ciencia").hover(function(){$(".list1").show();
+$("#science").hide();}, function(){$(".list1").hide();
+$("#science").show();});
+ 
+  $("#historia").hover(function(){$(".list2").show();
+$("#history").hide();}, function(){$(".list2").hide();
+$("#history").show();});
+ 
+  $("#regiones").hover(function(){$(".list3").show();
+$("#region").hide();}, function(){$(".list3").hide();
+$("#region").show();});
+  });
+ 
